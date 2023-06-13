692b1197fe0bbc40fac6542db4e9c6073fd25eaf
Javokhir Isomurodov <mr.isomurodov@gmail.com>
2023-06-14 00:24:36 +0200

    reset password

--- curiosity/routes.py
+++ curiosity/routes.py
@@ -1,11 +1,14 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from curiosity import app, db, bcrypt
-from curiosity.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from curiosity.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
+                             RequestResetForm, ResetPasswordForm)
 from curiosity.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+import sendgrid
+from sendgrid.helpers.mail import Mail
 
 @app.route("/")
 @app.route("/home")
@@ -88,3 +91,65 @@ def account():
     image_file = url_for('static', filename='assets/profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+
+    recipient = request.form.get('{{user.email.data}}')
+    subject = request.form.get('subject')
+    body = request.form.get(f'''To reset your password, visit the following link:
+{url_for('reset_token', token=token, _external=True)}
+
+If you did not make this request then simply ignore this email and no changes will be made.
+''')
+
+    # Create the SendGrid client
+    sg = sendgrid.SendGridAPIClient(api_key='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')
+
+    # Create the email message
+    message = Mail(
+        from_email='mr.isomurodov@gmail.com',
+        to_emails=recipient,
+        subject=subject,
+        plain_text_content=body
+    )
+
+    # Send the email
+    response = sg.send(message)
+
+    if response.status_code == 202:
+        return 'Email sent successfully!'
+    else:
+        return 'Failed to send email'
+
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.', 'info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', title='Reset Password', form=form)
+
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token', 'warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been updated! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', title='Reset Password', form=form)
