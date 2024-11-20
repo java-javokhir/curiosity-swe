ce39b2aa55a81e0ad71d7578853b1da7ecea5fa8
Javokhir (Java) Isomurodov <44898055+java-javokhir@users.noreply.github.com>
2024-11-20 15:24:48 +0100

    Update config.py

--- curiosity/config.py
+++ curiosity/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    SECRET_KEY = '47c8e5c73ae32be62cf1fcb4e862a766'
+    SECRET_KEY = ''
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    API_KEY = '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'
-    EMAIL_SENDER = 'mr.isomurodov@gmail.com'
+    API_KEY = ''
+    EMAIL_SENDER = ''
